d276e24345087b8b49368c1ad763d46aa479df36
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2012-11-17 10:19:28 +1100

    Moving further through the file. Trying to parse more of the logical screen descriptor.

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ var parseGif = function(path, cb) {
 
     pos = 6
 
+    // Logical screen width, 2 bits unsigned ints
     var lsw = []
     lsw[0] = buffer.readUInt8(pos)
     pos++
@@ -26,12 +27,42 @@ var parseGif = function(path, cb) {
     pos++
     gif.logical_screen_width = lsw[1] + lsw[0]
 
+    // Logical screen height, 2 bits unsigned ints
     var lsh = []
     lsh[0] = buffer.readUInt8(pos)
     pos++
     lsh[1] = buffer.readUInt8(pos) << 8
     pos++
     gif.logical_screen_height = lsh[1] + lsh[0]
+
+    // Following fields are packed
+    //  Global color table flag 1 bit
+    //  Color resolution 3 bits
+    //  Sort flag 1 bit
+    //  Size of GCT 3 bits
+    var packed = buffer[pos]
+    var a = []
+    for (var i = 7; i >= 0; i--) {
+      a.push(!!(packed & (1 << i)))
+    }
+    // Global color table flag:
+    //  true:   No Global Color Table follows, the Background
+    //          Color Index field is meaningless.
+    //  false:  A Global Color Table will immediately follow, the
+    //          Background Color Index field is meaningful.
+    gif.gct_flag = a.shift()
+    gif.color_res = a.splice(0,3).reduce(function(s, n) {
+      return s * 2 + n
+    }, 0)
+    gif.sort_flag = a.shift()
+    gif.gct_size = a.splice(0,3).reduce(function(s, n) {
+      return s * 2 + n
+    }, 0)
+    pos++
+
+    // TODO make use of gct flag to know whether to expect gct now or not
+
+
 
     cb(gif)
   })
--- test/test.js
+++ test/test.js
@@ -5,13 +5,20 @@ describe('parseGif', function() {
   it("should return gif data", function(done) {
     parseGif('./test/fixtures/syddir-1.gif', function(gif) {
 
+      console.log(gif)
+
       assert.equal(gif.signature, 'GIF')
       assert.equal(gif.version, '87a')
 
-      console.log(gif)
-
       assert.equal(gif.logical_screen_width, 603)
       assert.equal(gif.logical_screen_height, 751)
+
+      // NOTE I don't actually know what these values should be...
+      //      for now I'll assume it's OK. Will see later if I'm right
+      assert.equal(gif.gct_flag, true)
+      assert.equal(gif.color_res, 0)
+      assert.equal(gif.sort_flag, false)
+      assert.equal(gif.gct_size, 3)
 
       done()
     })
