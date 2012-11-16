549c4d8a122db9923089337f110576570cab059a
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2012-11-17 09:54:51 +1100

    Starting with a gif decoder.

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-var mhl = {}
+var fs = require('fs')
 
-mhl.parseImage = function(path) {
-  return "foo"
+var getImageData = function(path, cb) {
+  fs.readFile(path, function(err, data) {
+    if (err) {
+      return console.log(err)
+    }
+    return cb(data)
+  })
 }
 
-module.exports = mhl
+var parseGif = function(path, cb) {
+  getImageData(path, function(buffer) {
+    var gif = {},
+        pos = 0
+
+    gif.signature = buffer.toString('utf8', 0, 3)
+    gif.version = buffer.toString('utf8', 3, 6)
+
+    pos = 6
+
+    var lsw = []
+    lsw[0] = buffer.readUInt8(pos)
+    pos++
+    lsw[1] = buffer.readUInt8(pos) << 8
+    pos++
+    gif.logical_screen_width = lsw[1] + lsw[0]
+
+    var lsh = []
+    lsh[0] = buffer.readUInt8(pos)
+    pos++
+    lsh[1] = buffer.readUInt8(pos) << 8
+    pos++
+    gif.logical_screen_height = lsh[1] + lsh[0]
+
+    cb(gif)
+  })
+}
+
+module.exports = parseGif
--- test/test.js
+++ test/test.js
@@ -1,14 +1,19 @@
-var mhl = require('../')
+var parseGif = require('../')
 var assert = require('assert')
 
-describe('mhl', function() {
-  it("should return wave data", function() {
-    var data = mhl.parseImage('fixtures/syddir-1.gif')
+describe('parseGif', function() {
+  it("should return gif data", function(done) {
+    parseGif('./test/fixtures/syddir-1.gif', function(gif) {
 
-    assert(data.direction, 'direction present')
-    assert(data.size_max, 'max size present')
-    assert(data.size_sig, 'signal size present')
-    assert(data.period_max, 'max size present')
-    assert(data.period_sig, 'signal size present')
+      assert.equal(gif.signature, 'GIF')
+      assert.equal(gif.version, '87a')
+
+      console.log(gif)
+
+      assert.equal(gif.logical_screen_width, 603)
+      assert.equal(gif.logical_screen_height, 751)
+
+      done()
+    })
   })
 })
