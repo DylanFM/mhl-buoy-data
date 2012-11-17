8fe078c2739657c0a0d83122067fd24ea56ebde4
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2012-11-17 14:16:53 +1100

    Some different test gifs in fixtures and something that appears to be parsing the global colour table.

--- test/test.js
+++ test/test.js
@@ -3,23 +3,25 @@ var assert = require('assert')
 
 describe('parseGif', function() {
   it("should return gif data", function(done) {
-    parseGif('./test/fixtures/syddir-1.gif', function(gif) {
+    parseGif('./test/fixtures/pretty.gif', function(gif) {
 
       console.log(gif)
 
       assert.equal(gif.signature, 'GIF')
-      assert.equal(gif.version, '87a')
+      assert.equal(gif.version, '89a')
 
-      assert.equal(gif.logical_screen_width, 603)
-      assert.equal(gif.logical_screen_height, 751)
+      assert.equal(gif.logical_screen_width, 3)
+      assert.equal(gif.logical_screen_height, 3)
 
       // NOTE I don't actually know what these values should be...
-      assert.equal(gif.gct_flag, '')
-      assert.equal(gif.color_res, '')
-      assert.equal(gif.sort_flag, '')
-      assert.equal(gif.gct_size, '')
-      assert.equal(gif.bg_index, '')
-      assert.equal(gif.pixel_aspect_ratio, '')
+      assert.equal(gif.gct_flag, true)
+      assert.equal(gif.color_res, '') // ? in spec but not in wikipedia explanation
+      assert.equal(gif.sort_flag, false)
+      assert.equal(gif.gct_size, 6)
+      assert.equal(gif.bg_index, 0) // 1st in color table, black
+      assert.equal(gif.pixel_aspect_ratio, 0)
+
+      assert.equal(gif.gct.length, 6)
 
       done()
     })
